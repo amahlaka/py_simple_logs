f0c5620d617299abd131f1d88411fe8b1a62043a
Arttu mahlakaarto <arttu.mahlakaarto@gmail.com>
2018-08-24 15:34:04 +0300

    added url

--- setup.py
+++ setup.py
@@ -15,9 +15,9 @@ setup(
     version=0.1,
     author='Arttu Mahlakaarto',
     author_email='Arttu.mahlakaarto@gmail.com',
-    url='http://github.com/amahlaka/',
+    url='http://github.com/amahlaka/py_simple_logs',
     packages=find_packages(),
-    download_url='https://github.com/amahlaka//tarball/0.1',
+    download_url='https://github.com/amahlaka/py_simple_logs/tarball/0.1',
     scripts=[],
     description='Wrapper that simplifies logging',
     classifiers=[
